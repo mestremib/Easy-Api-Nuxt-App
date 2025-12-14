eeeb5ab17d04b7fca645d7936cc7ddfcaf0fca32
Tiago França <github@tiagofranca.com>
2025-12-13 22:32:00 -0300

    fix cors #3
    	modified:   nuxt.config.ts
    	modified:   server/middleware/cors.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
             '/api/**': {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization, priority',
                 },
             },
         },
--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,13 +1,12 @@
-// server/middleware/cors.ts
+// server/middleware/api-cors.ts
 export default defineEventHandler((event) => {
-    // if (!event.path.startsWith('/api')) {
-    //     return;
-    // }
+    if (!event.path.startsWith('/api')) {
+        return;
+    }
 
-    // setHeader(event, 'Access-Control-Allow-Origin', 'https://example.com');
     setHeader(event, 'Access-Control-Allow-Origin', '*');
-    setHeader(event, 'Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization, priority');
+    setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 
     if (event.method === 'OPTIONS') {
         event.node.res.statusCode = 204;
