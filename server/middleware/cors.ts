904a776f780deb98e2f2cef6233f7da289d5ba9c
Tiago França <github@tiagofranca.com>
2025-12-13 22:36:33 -0300

    allow cors 	modified:   server/middleware/cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,12 +1,14 @@
-// server/middleware/api-cors.ts
+const ALLOWED_HEADERS =
+    'Content-Type, Authorization, Accept, Accept-Language, Origin, User-Agent, Priority';
+
 export default defineEventHandler((event) => {
     if (!event.path.startsWith('/api')) {
         return;
     }
 
     setHeader(event, 'Access-Control-Allow-Origin', '*');
-    setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization, priority');
-    setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    setHeader(event, 'Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    setHeader(event, 'Access-Control-Allow-Headers', ALLOWED_HEADERS);
 
     if (event.method === 'OPTIONS') {
         event.node.res.statusCode = 204;
